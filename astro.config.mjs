b5a28fc992a9e02121fa4207bb4932e5c63827ad
hammadxcm <hammadkhanxcm@gmail.com>
2026-03-01 00:55:22 +0500

    Configure Astro SCSS integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://hammadxcm.github.io',
   base: '/hammadxcm',
   compressHTML: true,
+  integrations: [sitemap()],
 });
